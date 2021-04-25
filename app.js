98d9adef88a1afac0d6b3ce27fb552347a4be970
naoki <n.globe.us@gmail.com>
2021-04-25 11:02:20 +0900

    Fix some

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useLayoutEffect } from "react"
 import {render} from 'react-dom';
-import {Deck} from '@deck.gl/core';
+import DeckGL from '@deck.gl/react';
 import {Tile3DLayer} from '@deck.gl/geo-layers';
+
 import {Tiles3DLoader} from '@loaders.gl/3d-tiles';
 
 const TILESET_URL = `./tileset.json`;
@@ -21,32 +22,8 @@ export default function App({
   updateAttributions
 }) {
   const [initialViewState, setInitialViewState] = useState(INITIAL_VIEW_STATE);
-  const mapNode = useRef(null)
   const mapDiv = useRef(null)
-  const deckDiv = useRef(null)
-
-  const onTilesetLoad = tileset => {
-    // Recenter view to cover the new tileset
-    const {cartographicCenter, zoom} = tileset;
-    setInitialViewState({
-      ...INITIAL_VIEW_STATE,
-      longitude: cartographicCenter[0],
-      latitude: cartographicCenter[1],
-      zoom
-    });
-
-    if (updateAttributions) {
-      updateAttributions(tileset.credits && tileset.credits.attributions);
-    }
-  };
-
-  const tile3DLayer = new Tile3DLayer({
-    id: 'tile-3d-layer',
-    pointSize: 2,
-    data: TILESET_URL,
-    loader: Tiles3DLoader,
-    onTilesetLoad
-  });
+  const mapNode = useRef(null)
 
   // @ts-ignore
   const { geolonia } = window
@@ -55,54 +32,73 @@ export default function App({
     if (!mapDiv.current) { return }
     if (mapNode.current !== null) { return }
 
-    mapNode.current = new geolonia.Map(
-      {
-        container: mapDiv.current,
-        style: 'geolonia/homework',
-        interactive: true,
-        center: [initialViewState.longitude, initialViewState.latitude],
-        zoom: initialViewState.zoom,
-        bearing: initialViewState.bearing,
-        pitch: initialViewState.pitch,
-      })
+    mapNode.current = new geolonia.Map({
+      container: mapDiv.current,
+      style: 'geolonia/basic',
+      interactive: true,
+      center: [initialViewState.longitude, initialViewState.latitude],
+      bearing: initialViewState.bearing,
+      pitch: initialViewState.pitch,
+      maxPitch: initialViewState.maxPitch,
+      minZoom: initialViewState.minZoom,
+      maxZoom: initialViewState.maxZoom,
+      zoom: initialViewState.zoom,
+    })
   }, [mapDiv, geolonia])
 
-  useLayoutEffect(() => {
-    if (!deckDiv.current) { return }
+  const onTilesetLoad = tileset => {
+    // Recenter view to cover the new tileset
+    const {cartographicCenter, zoom} = tileset;
+    const lon = cartographicCenter[0]
+    const lat = cartographicCenter[1]
 
-    new Deck({
-      container: deckDiv.current,
-      width: '100%',
-      height: '100%',
-      initialViewState: initialViewState,
-      controller: true,
-      onViewStateChange: ({ viewState }) => {
-        mapNode.current.jumpTo({
-          center: [viewState.longitude, viewState.latitude],
-          zoom: viewState.zoom,
-          bearing: viewState.bearing,
-          pitch: viewState.pitch,
-        })
-      },
-      layers: [tile3DLayer],
+    setInitialViewState({
+      ...INITIAL_VIEW_STATE,
+      longitude: lon,
+      latitude: lat,
+      zoom
+    });
+
+    mapNode.current.jumpTo({
+      center: [lon, lat],
+      zoom,
     })
 
-  }, [deckDiv, mapNode])
-  
+    if (updateAttributions) {
+      updateAttributions(tileset.credits && tileset.credits.attributions);
+    }
+  };
+
+  const onViewStateChange = ({viewState}) => {
+    mapNode.current.jumpTo({
+      center: [viewState.longitude, viewState.latitude],
+      zoom: viewState.zoom,
+      bearing: viewState.bearing,
+      pitch: viewState.pitch,
+    })
+  }
+
+  const tile3DLayer = new Tile3DLayer({
+    id: 'tile-3d-layer',
+    pointSize: 2,
+    data: TILESET_URL,
+    loader: Tiles3DLoader,
+    pickable: true,
+    onTilesetLoad,
+    onClick: (info, event) => console.log('Clicked:', info, event)
+  });
+
   return (
     <>
-        <div id="container">
-          <div
-            ref={mapDiv}
-            id="map"
-          />
-          <div 
-            ref={deckDiv}
-            id="deck"
-          />
-        </div>
+      <div
+        ref={mapDiv}
+        id="map"
+        data-navigation-control="off"
+      />
+      <DeckGL layers={[tile3DLayer]} initialViewState={initialViewState} onViewStateChange={onViewStateChange} controller={true}>
+      </DeckGL>
     </>
-  )
+  );
 }
 
 export function renderToDOM(container) {
